a0e4cd60885e3b26cf91fd7d749ce806c9f56b04
chespi1833 <chespicna@gmail.com>
2020-10-24 14:38:01 -0300

    primeros pasos con Vue

--- script/app.js
+++ script/app.js
@@ -1,8 +1,12 @@
-
-
 var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hola Juli!'
+  el: '#app',
+  data() {
+    return {
+      name: 'Bitcoin',
+      img: 'https://icons.iconarchive.com/icons/froyoshark/enkel/128/Bitcoin-icon.png',
+      changePercent: 1
     }
-  })
+  },
+  methods: {
+  }
+})
